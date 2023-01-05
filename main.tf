ac8ea70d0c89c5c42acdb0fd42c9aca4374b5b2f
Ruben Papovyan <rpuserh@gmail.com>
2023-01-05 13:41:25 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,29 @@
-variable "random_count" {
+variable "env_name" {
+  type = string
+}
+variable "redis_capacity" {
   type = number
 }
 
-resource "random_id" "this" {
-  count = var.random_count
-
-  byte_length = 8
+resource "azurerm_resource_group" "this" {
+  name     = "${var.env_name}-rg"
+  location = "West US 2"
 }
 
-output "ids" {
-  value = random_id.this.*.id
+resource "azurerm_redis_cache" "this" {
+  name                = "${env_name}-cache"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+  capacity            = var.redis_capacity
+  family              = "C"
+  sku_name            = "Standard"
+  enable_non_ssl_port = false
+  minimum_tls_version = "1.2"
+
+  redis_configuration {
+  }
+}
+
+output "redis_name" {
+  value = azurerm_redis_cache.this.name
 }
